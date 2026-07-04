e79478fb6ae6cbb9635dc4bb992cb69b447f3e58
Zenilton Sá <zenilton.dev@gmail.com>
2026-07-04 11:56:41 -0300

    feat: add webhook verification endpoint for Cloud API
    - Added a GET `/cloud-api` route to handle webhook verification using `hub.mode`, `hub.verify_token`, and `hub.challenge`.
    - Defined `VERIFY_TOKEN` constant for token validation.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const VERIFY_TOKEN = "ZZ429683C4C977415CAAFCCE10F7D57E11";
 
 // Middleware para interpretar JSON
 app.use(express.json());
@@ -10,6 +11,20 @@ app.post("/webhook", (req, res) => {
   console.log(req.body); // já é um objeto JSON
 
   res.status(200).json({ message: "Webhook received successfully." });
+});
+
+// Verificação do webhook
+app.get("/cloud-api", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("Webhook verificado!");
+    return res.status(200).send(challenge);
+  }
+
+  return res.sendStatus(403);
 });
 
 const PORT = process.env.PORT || 3000;
